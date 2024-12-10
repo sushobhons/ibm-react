9111a9beb06ca7997afeb2bf71e54cf2d0a85dfe
Sushobhon Sahoo <sushobhons@gmail.com>
2024-12-10 08:49:34 -0500

    Added custom_hook project folder

---
+++ custom_hook/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
+
+function App() {
+    return (
+        <>
+            <FetchData />
+            <h1>Yoga </h1>
+            <FetchYogaData />
+        </>
+    )
+}
+
+export default App
